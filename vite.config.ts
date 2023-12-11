5bba20f301c99803df652ab6c0a437d49b64d8d6
FefAzvdo <wfernandoaugusto01@gmail.com>
2023-12-11 16:24:38 -0300

    ajustes global

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +10,6 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+    host: "localhost",
   },
 });
